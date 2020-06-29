9c7dc49525193e62c2f661c472330b90bebc89d6
Nicholas Gingerella <nicholas.gingerella@gmail.com>
2020-06-28 20:28:02 -0700

    Complete lesson 2. Remove binaries

--- lessons/lesson2/src/main.cpp
+++ lessons/lesson2/src/main.cpp
@@ -6,6 +6,9 @@
 
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
+const int PAUSE_TIME_IN_SECONDS = 10;
+const int MILLISECONDS_IN_SECONDS = 1000;
+const int SDL_RENDERER_FIRST_AVAILABLE_DRIVER = -1;
 
 // Log an SDL error with some error message to the output stream
 // of our choice.
@@ -17,15 +20,16 @@ void logSDLError(std::ostream &os, const std::string &msg)
     os << msg << SDL_GetError() << std::endl;
 }
 
+// Load texture from file
 SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren)
 {
     SDL_Texture *texture = nullptr;
-    SDL_Surface *loadedImage = SDL_LoadBMP(file.c_str());
+    SDL_Surface *loaded_image = SDL_LoadBMP(file.c_str());
 
-    if (loadedImage != nullptr)
+    if (loaded_image != nullptr)
     {
-        texture = SDL_CreateTextureFromSurface(ren, loadedImage);
-        SDL_FreeSurface(loadedImage);
+        texture = SDL_CreateTextureFromSurface(ren, loaded_image);
+        SDL_FreeSurface(loaded_image);
 
         if (texture == nullptr)
         {
@@ -40,6 +44,8 @@ SDL_Texture* loadTexture(const std::string &file, SDL_Renderer *ren)
     return texture;
 }
 
+
+// Render a texture at a position on the screen
 void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y)
 {
     // Setup destination rectangle and place it at desired (x,y) location
@@ -52,72 +58,69 @@ void renderTexture(SDL_Texture *tex, SDL_Renderer *ren, int x, int y)
     SDL_RenderCopy(ren, tex, NULL, &dst);
 }
 
+
 int main(int argc, char **argv)
 {
-    const int SDL_RENDERER_FIRST_AVAILABLE_DRIVER = -1;
-
     if (SDL_Init(SDL_INIT_VIDEO) != 0)
     {
-        std::cerr << "SDL_init error: " << SDL_GetError() << std::endl;
+        logSDLError(std::cout, "SDL_init error");
         return 1;
     }
 
-    SDL_Window *win = SDL_CreateWindow("Hello World!", 100, 100, 640, 480, SDL_WINDOW_SHOWN);
-    if (win == nullptr)
+    SDL_Window *window = SDL_CreateWindow("Hello World!", 100, 100, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    if (window == nullptr)
     {
-        std::cout << "SDL_CreateWindow Error: " << SDL_GetError() << std::endl;
+        logSDLError(std::cout, "SDL_CreateWindow Error");
         SDL_Quit();
         return 1;
     }
 
-    SDL_Renderer *ren = SDL_CreateRenderer(win, SDL_RENDERER_FIRST_AVAILABLE_DRIVER, SDL_RENDERER_ACCELERATED);
-    if (ren == nullptr)
+    SDL_Renderer *renderer = SDL_CreateRenderer(window,
+    SDL_RENDERER_FIRST_AVAILABLE_DRIVER,
+    SDL_RENDERER_ACCELERATED | SDL_RENDERER_PRESENTVSYNC);
+    if (renderer == nullptr)
     {
-        SDL_DestroyWindow(win);
-        std::cout << "SDL_CreateRenderer Error: " << SDL_GetError() << std::endl;
+        cleanup(window);
+        logSDLError(std::cout, "SDL_CreateRenderer Error");
         SDL_Quit();
         return 1;
     }
 
-    std::string imagePath = get_resource_path("lesson2") + "hello.bmp";
-    SDL_Surface *bmp = SDL_LoadBMP(imagePath.c_str());
-    if (bmp == nullptr)
+
+    // Load images
+    const std::string resource_path = get_resource_path("lesson2");
+    SDL_Texture *bg_texture = loadTexture(resource_path + "background.bmp", renderer);
+    SDL_Texture *img_texture = loadTexture(resource_path + "image.bmp", renderer);
+    if ( (bg_texture == nullptr) || (img_texture == nullptr) )
     {
-        SDL_DestroyRenderer(ren);
-        SDL_DestroyWindow(win);
-        std::cout << "SDL_LoadBMP Error: " << SDL_GetError() << std::endl;
+        cleanup(bg_texture, img_texture, renderer, window);
         SDL_Quit();
         return 1;
     }
 
-    SDL_Texture *tex = SDL_CreateTextureFromSurface(ren, bmp);
-    SDL_FreeSurface(bmp);
-    if (tex == nullptr)
+    // Render tiled background to window
+    int bg_width, bg_height;
+    SDL_QueryTexture(bg_texture, NULL, NULL, &bg_width, &bg_height);
+    for (int bg_pos_x = 0; bg_pos_x < SCREEN_WIDTH; bg_pos_x += bg_width)
     {
-        SDL_DestroyRenderer(ren);
-        SDL_DestroyWindow(win);
-        std::cout << "SDL_CreateTextureFromSurface Error: " << SDL_GetError() << std::endl;
-        SDL_Quit();
-        return 1;
+        for (int bg_pos_y = 0; bg_pos_y < SCREEN_HEIGHT; bg_pos_y += bg_height)
+        {
+            renderTexture(bg_texture, renderer, bg_pos_x, bg_pos_y);
+        }
     }
 
-    // Render
-    for (int i=0; i < 3; ++i)
-    {
-        // Clear the renderer
-        SDL_RenderClear(ren);
+    // Render image to center of window
+    int img_width, img_height;
+    SDL_QueryTexture(img_texture, NULL, NULL, &img_width, &img_height);
+    int img_pos_x = (SCREEN_WIDTH / 2) - (img_width / 2);
+    int img_pos_y = (SCREEN_HEIGHT / 2) - (img_height/ 2);
+    renderTexture(img_texture, renderer, img_pos_x, img_pos_y);
 
-        // Draw the texture
-        SDL_RenderCopy(ren, tex, NULL, NULL);
+    // Render current contents and pause
+    SDL_RenderPresent(renderer);
+    SDL_Delay(PAUSE_TIME_IN_SECONDS * MILLISECONDS_IN_SECONDS);
 
-        // Update the screen
-        SDL_RenderPresent(ren);
-
-        // Delay the closing of the window so we can see stuff
-        SDL_Delay(1000);
-    }
-
-    cleanup(tex, ren, win);
+    cleanup(bg_texture, img_texture, renderer, window);
     SDL_Quit();
     return 0;
 }
